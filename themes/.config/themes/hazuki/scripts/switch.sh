7db0af263092afc304fc84b5fff6f16389fb6d54
storedbyte <storedbyte+github@gmail.com>
2026-06-20 21:27:29 -0400

    fuzzel template with theme injection script for quick theme switching

--- themes/.config/themes/hazuki/scripts/switch.sh
+++ themes/.config/themes/hazuki/scripts/switch.sh
@@ -17,6 +17,16 @@ fi
 # swap the active symlink
 ln -sfn "$THEMES_DIR/$FLAVOR" "$ACTIVE"
 
+# template inject and symlink
+source "$ACTIVE/colors.sh"
+sed 	-e "s/{{bg-main}}/$bg_main/g" \
+	-e "s/{{bg-ui}}/$bg_ui/g" \
+	-e "s/{{bg-sel}}/$bg_sel/g" \
+	-e "s/{{fg}}/$fg/g" \
+	-e "s/{{ui-accent}}/$ui_accent/g" \
+	-e "s/{{ui-border}}/$ui_border/g" \
+	"$HOME/.config/themes/templates/fuzzel.ini" > "$HOME/.config/fuzzel/fuzzel.ini"
+
 # reload each app
 kill -SIGURS1 $(pidof kitty) 2>/dev/null
 niri msg action reload-config 2>/dev/null
